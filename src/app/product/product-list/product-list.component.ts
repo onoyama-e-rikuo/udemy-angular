19b62646b0f5fc61380fe9bcbeb4e00ffca8ba51
Onoyama Rikuo <evang00dd@gmail.com>
2020-07-27 22:22:13 +0900

    okay

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { products } from "../../products";
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-  products: any = [0,1,2,3];
+  products = products;
 
   constructor() { }
 
